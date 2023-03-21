0967fdfeb0ffaff44393c10cae37c76fde2b5d96
rlmarlma <rom1999@yandex.ru>
2023-03-22 00:03:17 +0300

    Domain layer created

---
+++ app/src/main/java/ru/rskryukov/android/shopinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,40 @@
+package ru.rskryukov.android.shopinglist.data
+
+import ru.rskryukov.android.shopinglist.domain.ShopItem
+import ru.rskryukov.android.shopinglist.domain.ShopListRepository
+import java.lang.RuntimeException
+
+object ShopListRepositoryImpl : ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addItem(item: ShopItem) {
+        if (item.id == ShopItem.UNDEFINED_ID) {
+            item.id = autoIncrementId++
+        }
+        shopList.add(item)
+    }
+
+    override fun deleteItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editItem(shopItem: ShopItem) {
+        val oldElement = getItem(shopItem.id)
+        shopList.remove(oldElement)
+        addItem(shopItem)
+    }
+
+    override fun getItem(id: Int): ShopItem {
+        return shopList.find {
+            it.id == id
+        } ?: throw RuntimeException("Element with id $id not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toList()
+    }
+
+}
